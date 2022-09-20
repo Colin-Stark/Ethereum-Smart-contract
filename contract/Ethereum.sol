8b7f6f2da4c28e1e5160a209fdb6e26f151d4959
Colin Stark <collinscodes@gmail.com>
2022-09-19 22:33:10 -0800

    Update the contract to work optimally
    i removed the following variables(hashdigit and hashmodulus) from being state variable as it will cost more gas to store and it is not a data that requires to be persistent
    the function verifyDigitCod was also redundant as your minified_Address variable basically stores the same thing and it could be called at any time

--- contract/Ethereum.sol
+++ contract/Ethereum.sol
@@ -15,14 +15,7 @@ contract ENS {
     mapping(address => bool) public wallet_Address;
 
     // A variable to store a 5 digit minified address as we will use it to protect against errors that can be generated from capitalization as James and james are two different things entirely
-    mapping(uint => address) minified_Address;
-
-    //We will be using this variable to store the amount of digit we will be using for minified address and since it is a constant we store it directly with 5
-     uint hashdigit = 5;
-
-     //Modulus variable to make it 10 ^ 8
-
-    uint hashModulus = 10 ** hashdigit;
+    mapping(uint => address) public minified_Address;
 
     /**
         @dev allows unregistered users to create a profile with a username
@@ -37,6 +30,13 @@ contract ENS {
             wallet_Address[msg.sender] == false,
             "This user has already been registered"
         );
+
+            //We will be using this variable to store the amount of digit we will be using for minified address and since it is a constant we store it directly with 5
+             uint hashdigit = 5;
+
+            //Modulus variable to make it 10 ^ 8
+
+             uint hashModulus = 10 ** hashdigit;
         //packing strings to byte 
          uint pack_string = uint(keccak256(abi.encodePacked(_username)));   
          uint min_Digits = pack_string % hashModulus;  
@@ -53,9 +53,16 @@ contract ENS {
 
     // Input the name to obtain your unique ID 
 
-    function getDigitCode(string memory _username) public view returns (uint){
+    function getDigitCode(string memory _username) public pure returns (uint){
 
        uint pack_string = uint(keccak256(abi.encodePacked(_username)));
+
+        //We will be using this variable to store the amount of digit we will be using for minified address and since it is a constant we store it directly with 5
+        uint hashdigit = 5;
+
+        //Modulus variable to make it 10 ^ 8
+
+        uint hashModulus = 10 ** hashdigit;
 
          uint min_Digits = pack_string % hashModulus;
 
@@ -74,7 +81,7 @@ contract ENS {
      * @param _myUniqueCode username of the user
      */
     function sendEth(uint _myUniqueCode) public payable {
-        address receiverAddress = verifyDigitCodeAddress(_myUniqueCode);
+        address receiverAddress = minified_Address[_myUniqueCode];
         require(receiverAddress != address(0), "Query of nonexistent user");
         (bool sent, ) = payable(receiverAddress).call{value: msg.value}("");
         require(sent, "Failed Transaction");
